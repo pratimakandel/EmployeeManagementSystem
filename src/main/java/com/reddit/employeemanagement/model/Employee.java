1b970076cef560a75204db86199e2858c368cddb
Laurence Hall <laurencehall1@outlook.com>
2021-02-13 11:22:48 -0500

    properties on Employee entity

--- src/main/java/com/reddit/employeemanagement/model/Employee.java
+++ src/main/java/com/reddit/employeemanagement/model/Employee.java
@@ -17,4 +17,11 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
 }
--- src/main/java/com/reddit/employeemanagement/service/EmployeeService.java
+++ src/main/java/com/reddit/employeemanagement/service/EmployeeService.java
@@ -1,4 +1,5 @@
 package com.reddit.employeemanagement.service;
 
+
 public interface EmployeeService {
 }
